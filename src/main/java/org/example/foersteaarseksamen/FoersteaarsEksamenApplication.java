4918a04e3b5197caa78f86814240e75ce770b25f
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-11-20 11:08:53 +0100

    Initial commit

---
+++ src/main/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplication.java
@@ -0,0 +1,13 @@
+package org.example.foersteaarseksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoersteaarsEksamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoersteaarsEksamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foersteaarseksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoersteaarsEksamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
