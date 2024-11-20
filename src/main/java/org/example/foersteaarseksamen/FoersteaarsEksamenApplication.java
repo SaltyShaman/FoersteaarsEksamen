2c72618c685f8c9e67645b0c73a0405fffa89925
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-11-20 11:43:45 +0100

    Spring datasource

--- src/main/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplication.java
+++ src/main/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoersteaarsEksamenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoersteaarsEksamenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoersteaarsEksamenApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplicationTests.java
+++ src/test/java/org/example/foersteaarseksamen/FoersteaarsEksamenApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoersteaarsEksamenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
