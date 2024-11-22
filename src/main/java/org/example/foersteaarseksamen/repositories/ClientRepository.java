8a9198e4f241b56c3f069041a5034b15080ed2eb
Lukas Balthazar <LBTurner@outlook.dk>
2024-11-22 21:26:51 +0100

    Template for the CRUD methods in repository

--- src/main/java/org/example/foersteaarseksamen/repositories/ClientRepository.java
+++ src/main/java/org/example/foersteaarseksamen/repositories/ClientRepository.java
@@ -1,8 +1,39 @@
 package org.example.foersteaarseksamen.repositories;
 
 
+import org.example.foersteaarseksamen.models.Client;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Repository
 public class ClientRepository {
+
+    private String url = System.getenv("url");
+    private String user = System.getenv("username");
+    private String password = System.getenv("password");
+
+    // Establishing the database connection
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public void addClient(Client client) throws SQLException {
+
+    }
+
+    public void removeClient(long clientID ) throws SQLException {
+
+    }
+
+    public void updateClient(Client client) throws SQLException {
+
+    }
+    public Client readClient(String clientName, String clientEmail) throws SQLException {
+        Client client = null;
+
+        return client;
+    }
 }
