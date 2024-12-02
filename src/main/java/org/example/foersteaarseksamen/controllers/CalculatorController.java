7b67bf1ea8d630fca8d1790c101e16fae58ea3c8
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-12-02 15:00:29 +0100

    Redirect to calculator tool after submit

--- src/main/java/org/example/foersteaarseksamen/controllers/CalculatorController.java
+++ src/main/java/org/example/foersteaarseksamen/controllers/CalculatorController.java
@@ -17,33 +17,19 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-/*
 
-    //inspired by ChatGPt dec 1
-    @GetMapping("/readClientId")
-    public String readClientId(@RequestParam String clientCompany, Model model) {
-        int client_id = calculatorService.getClientId(clientCompany);
-        model.addAttribute("client_id", client_id);
-        return "register-new-calculator";
-    }
-
-    //inspired by ChatGPt dec 1
-    @GetMapping("/readProjectId")
-    public String readProjectId(@RequestParam String projectName, Model model) {
-        int projectId = calculatorService.getProjectManagementId(projectName);
-        model.addAttribute("projectId", projectId);
-        return "register-new-calculator";
-    }
-
-
-*/
 
     @PostMapping("/createCalculatorPost")
     public String createCalculator(@RequestParam String calculatorName,
                                    @RequestParam int client_id,
                                    @RequestParam int projectManagementId) {
         calculatorService.createCalculator(calculatorName, client_id, projectManagementId);
-        return "redirect:/calculator-tool";
+        return "redirect:/calculatorTool";
+    }
+
+    @GetMapping("/calculatorTool")
+    public String calculatorTool(Model model) {
+        return "calculator-tool";
     }
 
     @GetMapping("/createCalculator")
