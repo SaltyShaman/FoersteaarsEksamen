14550f8dfb7d0762f4e29292f5db40c949846317
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-12-11 22:20:07 +0100

    Typos

--- src/main/java/org/example/foersteaarseksamen/controllers/EmployeeController.java
+++ src/main/java/org/example/foersteaarseksamen/controllers/EmployeeController.java
@@ -33,8 +33,6 @@ public class EmployeeController {
     }
 
 
-
-*/
     @GetMapping("/selectAllEmployees")
     public String selectAllEmployees(Model model) {
         model.addAttribute("employees", employeeService.ReadAllEmployees());
